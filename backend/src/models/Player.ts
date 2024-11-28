33b5cbe34b21a4e074d3f39989d152893e37781a
BlasFerreira <fblasfacundo@gmail.com>
2024-11-27 22:44:20 -0300

    modificacion modelo

--- backend/src/models/Player.ts
+++ backend/src/models/Player.ts
@@ -1,32 +1,21 @@
 import { Model, DataTypes } from 'sequelize';
 import sequelize from '../db';
 
-class Player extends Model {
-  public id!: number;
-  public name!: string;
-  public position!: string;
-  public club!: string;
-  public nationality!: string;
-  public rating!: number;
-}
-
-Player.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    name: DataTypes.STRING,
-    position: DataTypes.STRING,
-    club: DataTypes.STRING,
-    nationality: DataTypes.STRING,
-    rating: DataTypes.INTEGER,
+const Player = sequelize.define('Player',{
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: DataTypes.STRING,
+  position: DataTypes.STRING,
+  club: DataTypes.STRING,
+  nationality: DataTypes.STRING,
+  rating: DataTypes.INTEGER,
   },
   {
-    sequelize,
-    modelName: 'Player',
-  }
-);
+  sequelize,
+  modelName: 'Player',
+  });
 
 export default Player;
