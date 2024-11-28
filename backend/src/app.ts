368762e89d3218199a809cbcbc864bb1fe4350b4
BlasFerreira <fblasfacundo@gmail.com>
2024-11-28 20:33:53 -0300

    asdgfas

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,10 +6,10 @@ const PORT = 8080;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.setHeader('Content-Type','text/html');
-  res.send('<h1> Soy el endpoint raiz! :) </h1>');
-});
+// app.get('/', (req, res) => {
+//   res.setHeader('Content-Type','text/html');
+//   res.send('<h1> Soy el endpoint raiz! :) </h1>');
+// });
 
 app.listen(PORT,() => {
   console.log("Hola Blas, esoty escuchando en el "+ PORT);
