dd6d067a85d3ce09cd75bb4c13609d19fedefa74
BlasFerreira <fblasfacundo@gmail.com>
2024-11-28 19:03:38 -0300

    p

---
+++ backend/src/app.ts
@@ -0,0 +1,6 @@
+//app.ts
+// Importa las dependencias necesarias
+import express from "express";// Framework para manejar el servidor HTTP
+import playerRoutes from "./routes/playerRoutes"; // Importa las rutas de jugadores
+import sequelize from "./db"; // Conexión a la base de datos
+
