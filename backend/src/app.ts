97dde0da977f00cb33f234f240473b28d6d3b4a1
BlasFerreira <fblasfacundo@gmail.com>
2024-11-28 20:05:26 -0300

    dfgasdf

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,16 @@
 //app.ts
 // Importa las dependencias necesarias
-import express from "express";// Framework para manejar el servidor HTTP
-import playerRoutes from "./routes/playerRoutes"; // Importa las rutas de jugadores
-import sequelize from "./db"; // Conexión a la base de datos
+import express from 'express';// Framework para manejar el servidor HTTP
 
+const PORT = 8080;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.setHeader('Content-Type','text/html');
+  res.send('<h1> Soy el endpoint raiz! :) </h1>');
+});
+
+app.listen(PORT,() => {
+  console.log("Hola Blas, esoty escuchando en el "+ PORT);
+});
